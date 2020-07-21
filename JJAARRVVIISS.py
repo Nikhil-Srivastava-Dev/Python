2d9003eefd8a3359a694f2bc08e80ac241c0bc21
Nikhil Srivastava <68149981+Nikhil-Srivastava-Dev@users.noreply.github.com>
2020-07-21 14:19:33 +0530

    Jarvis-AI.py
    This an "under-development" AI program written in Python language.

--- JJAARRVVIISS.py
+++ JJAARRVVIISS.py
@@ -18,7 +18,7 @@ def screenShot():
     image.show()
 
 
-def wishMe():
+def wishMeSir():
     hour = int(datetime.datetime.now().hour)
     if hour >= 0 and hour <= 12:
         speak("Good Morning Sir")
@@ -29,6 +29,18 @@ def wishMe():
     else:
         speak("Good Evevning Sir")
         print("Good Evevning Sir")
+        
+def wishMeMaam():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour <= 12:
+        speak("Good Morning Ma'am")
+        print("Good Morning Ma'am")
+    elif hour >= 12 and hour <= 18:
+        speak("Good Afternoon Ma'am")
+        print("Good Afternoon Ma'am")
+    else:
+        speak("Good Evevning Ma'am")
+        print("Good Evevning Ma'am")       
 
 
 def takeCommand():
@@ -51,54 +63,125 @@ def takeCommand():
     return query   
 
 if __name__ == "__main__":
-    speak("Starting Jarvis ")
-    wishMe()
-    while True:
-
+    
+    speak("This programme is under development enter Yes to continue No to exit")
+    print("This programme is under development enter Yes to continue No to exit")
+    yesno = input("Enter Yes to continue, No to exit :::  ")
+    lo = yesno.lower()
+    if lo == "yes":
         query = takeCommand().lower()
-         
-        if 'wikipedia' in query:
-            speak("Searching wikipedia")
-            print("Searching wikipedia")
-            query = query.replace("wikipedia", "")            
-            results = wikipedia.summary(query, sentences=2)
-            speak("According to wikipedia")
-            print("According to wikipedia")
-            print(results)
-            speak(results)
 
-        elif 'nikita' in query:
-            print("Hello Ma'am, If I'm not wrong then you're sir's sister Nikita ?")
-            speak("Hello Ma'am, If I'm not wrong then you're sir's sister Nikita ?")                    
-        elif 'google' in query:
-            speak('opening google')
-            webbrowser.open('google.com')
-        elif 'youtube' in query:
-            speak('opening youtube')
-            webbrowser.open('youtube.com')
-        elif 'whatsapp' in query:
-            speak('opening whatsapp')
-            webbrowser.open('whatsapp.com')
-        elif 'jarvis' in query:
-            speak("Yes Sir , How may I help you ?")
-            print("Yes Sir , How may I help you ?")
-        elif 'how are you ?' in query:
-            speak("I am fine , tell me how are you ? ")
-            print("I am fine , tell me how are you ? ")
-        elif 'I am fine ' in query:
-            speak("Oh ! I just wanted to hear that ")
-            print("Oh ! I just wanted to hear that ")
-        elif 'good'in query:
-            speak("Thanks bhai , meri taarif karne ke liye tum bhi bahut achhe ho")
-        elif 'screenshot' in query:
-            speak("Taking screenshot of the current window")
-            screenShot()
-            speak("Taken the screenshot and now opening it")
-        elif 'bye' in query:
-            speak("Bye sir , have a nice day,")
-            print("Bye Sir, have a nice day,")
-            if hour > 19:
-                print("Bye sir and have a nice sleep")
-                speak("Bye sir and have a nice sleep")
-            break
+        speak("Please tell whether you are male or female ")
+        print("Please tell whether you are male or female ")
+        mf = input("Enter Male if you are male, or enter Female if you are female")
+        lmf = mf.lower()
+        speak("Ok thanks for telling me ")
+        speak("Starting Jarvis ")
+        if lmf == "male":
+            wishMe()
+            while True:
+                query = takeCommand().lower()
 
+
+                if 'wikipedia' in query:
+                    speak("Searching wikipedia")
+                    print("Searching wikipedia")
+                    query = query.replace("wikipedia", "")            
+                    results = wikipedia.summary(query, sentences=2)
+                    speak("According to wikipedia")
+                    print("According to wikipedia")
+                    print(results)
+                    speak(results)
+
+                elif 'nikita' in query:
+                    print("Hello Ma'am, If I'm not wrong then you're sir's sister Nikita ?")
+                    speak("Hello Ma'am, If I'm not wrong then you're sir's sister Nikita ?")                    
+                elif 'google' in query:
+                    speak('opening google')
+                    webbrowser.open('google.com')
+                elif 'youtube' in query:
+                    speak('opening youtube')
+                    webbrowser.open('youtube.com')
+                elif 'whatsapp' in query:
+                    speak('opening whatsapp')
+                    webbrowser.open('whatsapp.com')
+                elif 'jarvis' in query:
+                    speak("Yes Sir , How may I help you ?")
+                    print("Yes Sir , How may I help you ?")
+                elif 'how are you ?' in query:
+                    speak("I am fine , tell me how are you ? ")
+                    print("I am fine , tell me how are you ? ")
+                elif 'I am fine ' in query:
+                    speak("Oh ! I just wanted to hear that ")
+                    print("Oh ! I just wanted to hear that ")
+                elif 'good'in query:
+                    speak("Thankyou for praising me :")
+                elif 'screenshot' in query:
+                    speak("Taking screenshot of the current window")
+                    screenShot()
+                    speak("Taken the screenshot and now opening it")
+                elif 'bye' in query:
+                    speak("Bye sir , have a nice day,")
+                    print("Bye Sir, have a nice day,")
+                    if hour > 19:
+                        print("Bye sir and have a nice sleep")
+                        speak("Bye sir and have a nice sleep")
+                    break
+        elif lmf == "female":
+            wishMeMaam()
+            while True:
+                query = takeCommand().lower()
+
+
+                if 'wikipedia' in query:
+                    speak("Searching wikipedia")
+                    print("Searching wikipedia")
+                    query = query.replace("wikipedia", "")            
+                    results = wikipedia.summary(query, sentences=2)
+                    speak("According to wikipedia")
+                    print("According to wikipedia")
+                    print(results)
+                    speak(results)
+
+#                 elif 'nikita' in query:
+#                     print("Hello Ma'am, If I'm not wrong then you're sir's sister Nikita ?")
+#                     speak("Hello Ma'am, If I'm not wrong then you're sir's sister Nikita ?")                    
+                elif 'google' in query:
+                    speak('opening google')
+                    webbrowser.open('google.com')
+                elif 'youtube' in query:
+                    speak('opening youtube')
+                    webbrowser.open('youtube.com')
+                elif 'whatsapp' in query:
+                    speak('opening whatsapp')
+                    webbrowser.open('whatsapp.com')
+                elif 'jarvis' in query:
+                    speak("Yes Ma'am , How may I help you ?")
+                    print("Yes Ma'am , How may I help you ?")
+                elif 'how are you ?' in query:
+                    speak("I am fine , tell me how are you ? ")
+                    print("I am fine , tell me how are you ? ")
+                elif 'I am fine ' in query:
+                    speak("Oh ! I just wanted to hear that ")
+                    print("Oh ! I just wanted to hear that ")
+                elif 'good'in query:
+                    speak("Thankyou for praising me :)")
+                elif 'screenshot' in query:
+                    speak("Taking screenshot of the current window")
+                    screenShot()
+                    speak("Taken the screenshot and now opening it")
+                elif 'bye' in query:
+                    speak("Bye Ma'am , have a nice day,")
+                    print("Bye Ma'am, have a nice day,")
+                    if hour > 19:
+                        print("Bye Ma'am and have a nice sleep")
+                        speak("Bye Ma'am and have a nice sleep")
+                    break
+    elif lo == "no":
+        speak("Exiting the programme")
+        print("Invalid input !!!!!")            
+#         break
+    else:
+        
+        speak("Invalid input !!!!!")
+        print("Invalid input !!!!!")            
